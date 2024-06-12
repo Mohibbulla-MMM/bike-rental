ae13136b7d39dc8e9829bb0e2f47a3b9cf67df4e
mohibbulla mohibbulla <rockychain1020@gmail.com>
2024-06-12 21:18:25 +0600

    user services file added

---
+++ src/app/module/user/user.services.ts
@@ -0,0 +1,14 @@
+// get/find user by id
+const userGet = async () => {
+  return "result";
+};
+
+// get/find user by id
+const userUpdated = async () => {
+  return "result";
+};
+
+export const UserServices = {
+  userGet,
+  userUpdated,
+};

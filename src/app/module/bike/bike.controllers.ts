1bb3358889ac19230d685259fd829b677da7ddbc
mohibbulla mohibbulla <rockychain1020@gmail.com>
2024-06-13 15:40:02 +0600

    Bike controllers  added

---
+++ src/app/module/bike/bike.controllers.ts
@@ -0,0 +1,57 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+
+import { TBike } from "./bike.interface";
+import { BikeServices } from "./bike.services";
+
+// create bike (admin)
+const createBike = catchAsync(async (req, res) => {
+  const result = await BikeServices.createBikeInToDB(req.body);
+
+  sendResponse(res, {
+    message: "Bike added successfully",
+    data: result,
+  });
+});
+
+// get/find all bike (all user)
+const findAllBike = catchAsync(async (req, res) => {
+  const result = await BikeServices.findAllBikeInToDB();
+
+  sendResponse(res, {
+    message: "Bikes retrieved successfully",
+    data: result,
+  });
+});
+
+// updated bike (admin)
+const updatedBike = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const payload = req.body;
+  if (Object.keys(payload)?.length === 0) {
+    throw new Error("Updated Value empty");
+  }
+  const result = await BikeServices.updatedBikeFromDB(id, payload);
+  sendResponse(res, {
+    message: "Bike updated successfully",
+    data: result,
+  });
+});
+
+// delete bike (admin)
+const deletedBike = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await BikeServices.deletedBikeInToDB(id);
+
+  sendResponse(res, {
+    message: "Bike deleted successfully",
+    data: result,
+  });
+});
+
+export const BikeControllers = {
+  createBike,
+  findAllBike,
+  updatedBike,
+  deletedBike,
+};

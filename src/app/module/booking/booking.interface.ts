9f540d10f44d3b094830461b8307b83d1f48da00
mohibbulla mohibbulla <rockychain1020@gmail.com>
2024-06-13 17:13:23 +0600

    Bookin model added

--- src/app/module/booking/booking.interface.ts
+++ src/app/module/booking/booking.interface.ts
@@ -1,10 +1,12 @@
 import { Types } from "mongoose";
 
+type TreturnTime = Date | null;
+
 export type TBooking = {
   userId: Types.ObjectId;
   bikeId: Types.ObjectId;
   startTime: Date;
-  returnTime: Date;
+  returnTime: TreturnTime;
   totalCost: number;
   isReturned: boolean;
 };
---
+++ src/app/module/booking/booking.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { TBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<TBooking>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  bikeId: {
+    type: Schema.Types.ObjectId,
+    ref: "Bike",
+    required: true,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  returnTime: {
+    type: Date,
+    default: null,
+  },
+  totalCost: {
+    type: Number,
+    required: true,
+  },
+  isReturned: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Booking = model<TBooking>("Booking", bookingSchema);

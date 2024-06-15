10fd408dfbcd6174ce745f206a6fbfc51e75ae1a
mohibbulla mohibbulla <rockychain1020@gmail.com>
2024-06-15 12:19:36 +0600

    global error > handle mongoose validation error added

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -4,8 +4,10 @@ import config from "../config";
 import { ZodError } from "zod";
 import handleZodError from "../errors/handleZodError";
 import handleDuplicateError from "../errors/handleDuplicateError";
-import handleCastError from "../errors/handleCastError";
-import handleMongooseCastError from "../errors/handleCastError";
+import handleCastError from "../errors/handleMongooseCastError";
+import handleMongooseCastError from "../errors/handleMongooseCastError";
+import handleMongooseValidationError from "../errors/handleMongooseValidationError";
+import AppError from "../errors/AppError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let message = err?.message || "Something went wrong";
@@ -22,18 +24,22 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     errorSource = simplifiedError?.errorSource;
     message = simplifiedError?.message;
+  } else if (err?.name == "ValidationError") {
+    const simplifiedError = handleMongooseValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    errorSource = simplifiedError?.errorSource;
+    message = simplifiedError?.message;
+  } else if (err?.name == "CastError") {
+    const simplifiedError = handleMongooseCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    errorSource = simplifiedError?.errorSource;
+    message = simplifiedError?.message;
   } else if (err?.errorResponse?.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     errorSource = simplifiedError?.errorSource;
     message = simplifiedError?.message;
-  } 
-  else if (err?.name == "CastError") {
-    const simplifiedError = handleMongooseCastError(err);
-    statusCode = simplifiedError?.statusCode;
-    errorSource = simplifiedError?.errorSource;
-    message = simplifiedError?.message;
-  }
+  }  
   const x = err?.name;
   console.log({ x });
   return res.status(statusCode).json({
